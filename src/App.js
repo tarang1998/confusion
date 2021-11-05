c06fd3a32560439c9920cbe0b495a81fbf44c464
Tarang <tarangnair98@gmail.com>
2021-11-06 01:06:55 +0530

    Added packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { Component } from 'react/cjs/react.production.min';
 import MainComponent from './components/MainComponent';
+import './App.css'
 
 class  App extends Component{
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 
 ReactDOM.render(
