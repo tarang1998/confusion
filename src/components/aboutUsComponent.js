969ab12442aaaef9e013d80d5c089f691c41884a
tarang1998 <tarangnair98@gmail.com>
2022-12-26 17:52:22 +0530

    react redux forms

--- src/components/aboutUsComponent.js
+++ src/components/aboutUsComponent.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media , Button, Row, Col, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
 import { Link } from 'react-router-dom';
 
+
+
+
+
 function About(props) {
+
+    function handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        // event.preventDefault();
+    }
+
+    
 
     const leaders = props.leaders.map((leader) => {
         return (
@@ -26,6 +40,8 @@ function About(props) {
             
         );
     });
+
+
 
     return(
 
@@ -91,6 +107,94 @@ function About(props) {
                     <Media list>
                         {leaders}
                     </Media>
+                </div>
+            </div>
+
+
+            <div className="row row-content">
+                <div className="col-12">
+                    <h2>Join Our Team</h2>
+                </div>
+                <div className="col-12 col-md-9">
+                <LocalForm onSubmit={(values) => handleSubmit(values)}>
+
+                            <Row className="form-group mt-5">
+                                <Label htmlFor="firstname" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".firstname" id="firstname" name="firstname"
+                                        placeholder="First Name"
+                                        className="form-control"
+                                         />
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group mt-5">
+                                <Label htmlFor="lastname" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".lastname" id="lastname" name="lastname"
+                                        placeholder="Last Name"
+                                        className="form-control"
+                                         />
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group mt-5">
+                                <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
+                                <Col md={10}>
+                                    <Control.text model=".telnum" id="telnum" name="telnum"
+                                        placeholder="Tel. Number"
+                                        className="form-control"
+                                         />
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group mt-5">
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Control.text model=".email" id="email" name="email"
+                                        placeholder="Email"
+                                        className="form-control" />
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group mt-5">
+                                <Col md={{size: 6, offset: 2}}>
+                                    <div className="form-check">
+                                        <Label check>
+                                            <Control.checkbox model=".agree" name="agree"
+                                                className="form-check-input"
+                                                 /> {' '}
+                                                <strong>May we contact you?</strong>
+                                        </Label>
+                                    </div>
+                                </Col>
+                                <Col md={{size: 3, offset: 1}}>
+                                    <Control.select model=".contactType" name="contactType"
+                                        className="form-control">
+                                        <option>Tel.</option>
+                                        <option>Email</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group mt-5">
+                                <Label htmlFor="message" md={2}>Additional Details</Label>
+                                <Col md={10}>
+                                    <Control.textarea model=".message" id="message" name="message"
+                                        rows="12"
+                                        className="form-control" />
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group mt-5">
+                                <Col md={{size:10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                    Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                        
                 </div>
             </div>
         </div>
